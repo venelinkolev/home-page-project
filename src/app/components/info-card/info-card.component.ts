0968f89442d19e09df1d1b2af7c835848f49eef0
venelinkolev <venelinkolev@gmail.com>
2023-10-25 17:35:03 +0300

    Card Finish

--- src/app/components/info-card/info-card.component.ts
+++ src/app/components/info-card/info-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-info-card',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./info-card.component.css']
 })
 export class InfoCardComponent {
-
+  @Input() color: string = '';
+  @Input() total: string = '';
+  @Input() nameCard: string = '';
 }
