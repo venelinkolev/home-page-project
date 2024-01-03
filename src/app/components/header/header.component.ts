418ab3e8ca5c4c522aa0bd6147f5e1200f6f1dbe
venelinkolev <venelinkolev@gmail.com>
2024-01-03 13:41:51 +0200

    Menu Btn State Services

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
+import { state } from '@angular/animations';
 import { Component } from '@angular/core';
+import { map } from 'rxjs';
+import { StateManagementService } from 'src/app/states/state-management.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  private _currentState!: boolean;
+
+
+
+  constructor(public stateManagement: StateManagementService) {
+    stateManagement.menuBtnStatus$.subscribe(state => this._currentState = state);
+  }
+
+  menuBtn(): void {
+    if (!this._currentState) {
+      this.stateManagement.menuBtnState(true);
+      console.log(this._currentState);
+    } else {
+      this.stateManagement.menuBtnState(false);
+      console.log(this._currentState);
+    }
+  }
 }
--- src/app/states/state-management.service.ts
+++ src/app/states/state-management.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateManagementService {
 
+  private _mobileMenuBtn$ = new BehaviorSubject<boolean>(false);
+
+  menuBtnStatus$: Observable<boolean> = this._mobileMenuBtn$.asObservable();
+
   constructor() { }
+
+  menuBtnState(state: boolean): void {
+    this._mobileMenuBtn$.next(state);
+  }
 }
