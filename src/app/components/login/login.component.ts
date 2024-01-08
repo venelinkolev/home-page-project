1bcf21efa0146bb521c69eaf31a2dc8f7ca4ac64
venelinkolev <venelinkolev@gmail.com>
2024-01-08 10:49:07 +0200

    Mobile Login Header Sub Menu

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(public renderer: Renderer2) { }
+
+  openSubMenu(e: HTMLElement) {
+    const loginHeaderElement = e.parentElement?.parentElement;
+    console.log(loginHeaderElement);
+    const navHeaderElement = loginHeaderElement?.children[1];
+    console.log(navHeaderElement);
+    if (e?.children[0].className == 'caret-down') {
+      // console.log('Down');
+      this.renderer.removeClass(e.children[0], 'caret-down');
+      this.renderer.removeClass(e.children[1], 'caret-up');
+      this.renderer.addClass(e.children[0], 'caret-up');
+      this.renderer.addClass(e.children[1], 'caret-down');
+      this.renderer.setStyle(navHeaderElement, 'display', 'block');
+    } else if (e?.children[0].className == 'caret-up') {
+      // console.log('Up');
+      this.renderer.removeClass(e.children[1], 'caret-down');
+      this.renderer.removeClass(e.children[0], 'caret-up');
+      this.renderer.addClass(e.children[1], 'caret-up');
+      this.renderer.addClass(e.children[0], 'caret-down');
+      this.renderer.setStyle(navHeaderElement, 'display', 'none');
+    }
+  }
 }
